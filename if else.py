421c8ff94e58f62c340bbbe6a56b48fb1d866109
pallavihanumaradder <pallavihanumaradder@gmail.com>
2025-09-19 22:54:25 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,6 @@
+eng_marks=int(input("enter ur english marks"))
+sci_marks=int(input("enter ur science marks"))
+if eng_marks>25 and sci_marks>30:
+    print("ur elegible")
+else:
+    print("not elegible")
